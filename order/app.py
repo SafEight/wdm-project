d8c81aff0c986e464e40cbbb0804c4c567a631c6
Merlijn Mac Gillavry <merlijnmac@gmail.com>
2023-05-04 01:10:54 +0200

    Added basic order functionality
    All functions except for checkout are implemented. I also created a helper Order class that has some simple methods.

--- order/app.py
+++ order/app.py
@@ -3,7 +3,7 @@ import atexit
 
 from flask import Flask, jsonify
 import redis
-
+from orders import Order, order_from_JSON
 
 gateway_url = os.environ['GATEWAY_URL']
 
@@ -18,39 +18,60 @@ db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
 def close_db_connection():
     db.close()
 
+
 @app.get("/")
 def status():
     data = {
         "msg": "Success orders/ is online"
     }
-    return jsonify(data)
+    return jsonify(data), 200
+
 
 atexit.register(close_db_connection)
 
 
 @app.post('/create/<user_id>')
 def create_order(user_id):
-    pass
+    o = Order(user_id)
+    db.set(o.order_id, o.toJSON())
+    return jsonify({"order_id":  o.order_id}), 200
 
 
 @app.delete('/remove/<order_id>')
 def remove_order(order_id):
-    pass
+    db.delete(order_id)
+    return "", 200
 
 
 @app.post('/addItem/<order_id>/<item_id>')
 def add_item(order_id, item_id):
-    pass
+    order_json = db.get(order_id)
+    if order_json:
+        o = order_from_JSON(order_json)
+        o.add_item(item_id)
+        db.set(o.order_id, o.toJSON())
+        return f"Added item!: {item_id}", 200
+    return "Order does not exist!", 400
 
 
 @app.delete('/removeItem/<order_id>/<item_id>')
 def remove_item(order_id, item_id):
-    pass
+    order_json = db.get(order_id)
+    if order_json:
+        o = order_from_JSON(order_json)
+        o.remove_item(item_id)
+        db.set(o.order_id, o.toJSON())
+        return f"Removed item: {item_id}!", 200
+    return "Order does not exist!", 400
 
 
 @app.get('/find/<order_id>')
 def find_order(order_id):
-    pass
+    order_json = db.get(order_id)
+    if order_json:
+        o = order_from_JSON(order_json)
+        return o.toJSON(), 200
+    return "Order does not exist!", 400
 
 
 @app.post('/checkout/<order_id>')
---
+++ order/orders.py
@@ -0,0 +1,38 @@
+import uuid
+import json
+
+
+class Order:
+    def __init__(self, user_id):
+        self.order_id = str(uuid.uuid4())
+        self.paid = False
+        self.items = []
+        self.user_id = user_id
+        self.total_cost = 0
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+
+    def add_item(self, item_id):
+        self.items.append(item_id)
+
+    def remove_item(self, item_id):
+        self.items.remove(item_id)
+
+    def pay(self):
+        self.paid = True
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, Order):
+            return self.order_id == __o.order_id
+
+
+def order_from_JSON(json_string):
+    json_dict = json.loads(json_string)
+    order = Order(json_dict['user_id'])
+    order.order_id = json_dict['order_id']
+    order.paid = json_dict['paid']
+    order.items = json_dict['items']
+    order.total_cost = json_dict['total_cost']
+    return order
