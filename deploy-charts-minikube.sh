155e43df6481c37fc461e1c6e8c0bc3d2c4644ca
SafEight <saf@saf.ouane>
2023-05-24 14:07:04 +0200

    Script and yaml to build and run k8s locally

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+docker-compose build
+
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+minikube start
+helm upgrade --install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+
+minikube image load order:latest
+minikube image load stock:latest
+minikube image load user:latest
+
+kubectl apply -f k8s/
