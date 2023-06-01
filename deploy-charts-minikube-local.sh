f29fad96ceef0d89ae7edbbb7dbc89b8e6f05771
SafEight <saf@saf.ouane>
2023-06-01 10:10:58 +0200

    Sharding start (do not use)

--- deploy-charts-minikube-local.sh
+++ deploy-charts-minikube-local.sh
@@ -5,8 +5,11 @@ docker-compose build
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-minikube start
+# minikube start
 helm upgrade --install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+# for i in {7000..7007}; do
+#     redis-server helm-config/$i/redis.conf --daemonize yes
+# done
 
 minikube image load order:latest
 minikube image load stock:latest
