0ad1b248f78fedcf98b2fef21593edc5ffcf39c5
jdewater <j.j.p.dewater@student.tudelft.nl>
2023-06-01 13:52:25 +0200

    Add messagequeue handling and first implementation of create order method

--- order/app.py
+++ order/app.py
@@ -1,11 +1,20 @@
 import os
 import atexit
 import requests
+import uuid
+import pika
+import time
 
 from flask import Flask, jsonify
-from orders import Order, order_from_JSON
 
 app = Flask("order-service")
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+request_queue = "request_queue"
+response_queue = "response_queue"
+
+responses = {}
 
 @app.get("/")
 def status():
@@ -16,7 +25,16 @@ def status():
 
 @app.post('/create/<user_id>')
 def create_order(user_id): 
-    return 0
+    request_id = str(uuid.uuid4())
+
+    channel.basic_publish(exchange='', routing_key=request_queue, body={request_id, user_id})
+
+    response = wait_for_response(request_id)
+
+    if response:
+        return jsonify(response)
+    else:
+        return jsonify({'error': 'Timeout occurred'}), 500
 
 @app.delete('/remove/<order_id>')
 def remove_order(order_id):
@@ -37,3 +55,24 @@ def find_order(order_id):
 @app.post('/checkout/<order_id>')
 def checkout(order_id):
     return 0
+
+def wait_for_response(request_id):
+    start_time = time.time()
+    timeout = 0.35
+
+    while time.time() - start_time < timeout:
+        if request_id in responses:
+            return responses.pop(request_id)
+
+        time.sleep(0.01)
+
+    return None
+
+def handle_message(channel, method, properties, body):
+    request_id = body.decode()
+    responses[request_id] = {"msg": "Succesful response"}
+
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.queue_declare(queue=response_queue)
+channel.basic_consume(queue=response_queue, on_message_callback=handle_message)
